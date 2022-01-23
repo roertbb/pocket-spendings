0a542ee5d359fa6755d604f18661a3edd28d7a6b
roertbb <robert.banaszak1997@gmail.com>
2022-01-23 17:24:00 +0100

    revert changes to header

--- components/ui/Header/Header.tsx
+++ components/ui/Header/Header.tsx
@@ -1,12 +1,18 @@
+import { ReactNode } from "react";
+
 interface HeaderProps {
   children: string;
+  leftAction?: ReactNode;
+  rightAction?: ReactNode;
 }
 
-export function Header({ children }: HeaderProps) {
+export function Header({ children, leftAction, rightAction }: HeaderProps) {
   return (
     <header className="border-b border-gray-100 shadow-sm">
-      <div className="flex items-center justify-center max-w-2xl p-3 m-auto">
+      <div className="flex items-center justify-between max-w-2xl p-3 m-auto">
+        <div className="flex w-10">{leftAction}</div>
         <h1 className="text-2xl font-bold">{children}</h1>
+        <div className="flex w-10">{rightAction}</div>
       </div>
     </header>
   );
